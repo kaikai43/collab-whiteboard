a72516be25ddd339bd5c25f1889192d0fb42ea82
kaikai43 <jteh3@student.unimelb.edu.au>
2020-11-01 17:59:25 +1100

    Implements WhiteboardServer receiving message

--- src/main/java/pb/WhiteboardServer.java
+++ src/main/java/pb/WhiteboardServer.java
@@ -1,6 +1,9 @@
 package pb;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import org.apache.commons.cli.CommandLine;
@@ -11,7 +14,9 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
 
+import pb.app.WhiteboardApp;
 import pb.managers.ServerManager;
+import pb.managers.endpoint.Endpoint;
 import pb.utils.Utils;
 
 /**
@@ -80,8 +85,66 @@ public class WhiteboardServer {
 	 * Default port number.
 	 */
 	private static int port = Utils.indexServerPort;
-	
-	
+
+	/**
+	 * Storage of boards and their hosting Whiteboard peer
+	 * host:port -> boardId
+	 */
+	//public static final HashMap<String,Set<String>> keyValueMap=new HashMap<>();
+	public static final HashSet<String>  sharedBoards = new HashSet<String>();
+
+	/**
+	 * Get peer that is hosting a board
+	 * @param data: host:port:boardID
+	 * @return host:port
+	 */
+	private static String getBoardHost(String data){
+		String[] parts = data.split(":");
+		return parts[0] + ":" + parts[1];
+	}
+
+	/**
+	 * Get boardId for hosted board
+	 * @param data: host:port:boardID
+	 * @return boardId
+	 */
+	private static String getBoardId(String data){
+		String[] parts = data.split(":");
+		return parts[2];
+	}
+
+	/**
+	 * Insert newly hosted board into list of currently shared boards
+	 * when "shareBoard" event is received.
+	 * @param data: host:port:boardID
+	 */
+	private static void sharedBoardsInsert(String data){
+		synchronized (sharedBoards){
+			sharedBoards.add(data);
+		}
+/*		//HashMap version of same implementation
+		String host = getBoardHost(data);
+		String boardId = getBoardId(data);
+		synchronized (keyValueMap) {
+			if(!keyValueMap.containsKey(host)) {
+				keyValueMap.put(host, new HashSet<String>());
+			}
+			Set<String> sharedBoards=keyValueMap.get(host);
+			sharedBoards.add(boardId);
+		}
+*/
+	}
+
+	/**
+	 * Delete hosted board from list of currently shared boards
+	 * when "unshareBoard" event is received.
+	 * @param data: host:port:boardID
+	 */
+	private static void sharedBoardsDelete(String data) {
+		synchronized (sharedBoards) {
+			sharedBoards.remove(data);
+		}
+	}
 	
 	private static void help(Options options){
 		String header = "PB Whiteboard Server for Unimelb COMP90015\n\n";
@@ -132,6 +195,39 @@ public class WhiteboardServer {
         /**
          * TODO: Put some server related code here.
          */
+        // Should heavily reference IndexServer
+        // Jin: Listen to shareBoard event from any client, emit sharingBoard event to all other clients/all clients
+		serverManager.on(ServerManager.sessionStarted,(eventArgs)-> {
+			Endpoint endpoint = (Endpoint)eventArgs[0];
+			log.info("Client session started: "+endpoint.getOtherEndpointId());
+			log.info("Transmitting all currently shared boards to client.");
+			for (String board : sharedBoards){
+				serverManager.emit(sharingBoard, board);
+			}
+			endpoint.on(shareBoard, (eventArgs2)->{
+				String boardInfo = (String) eventArgs2[0];
+				String[] parts=boardInfo.split(":");
+				log.info("Board " + parts[2] + " is shared by: "+ parts[0] + ":" + parts[1]);
+				sharedBoardsInsert(boardInfo);
+				serverManager.emit(sharingBoard, boardInfo);
+			}).on(unshareBoard, (eventArgs2)->{
+				String boardInfo = (String) eventArgs2[0];
+				String[] parts=boardInfo.split(":");
+				log.info("Board " + parts[2] + " is unshared by: "+ parts[0] + ":" + parts[1]);
+				if (sharedBoards.contains(boardInfo)){
+					sharedBoardsDelete(boardInfo);
+				} else {
+					//Peer trying to unshare a board that does not exist
+					serverManager.emit(error, "Board does not exist.");
+				}
+				serverManager.emit(unsharingBoard, boardInfo);
+			});
+		});
+
+		// Jin: Listen to peerStarted event from new clients, emit multiple sharingBoard events to that client
+			// How do we keep track of all of the shared boards? maybe we add a Map of <Peer, Board>?
+		// Jin: Listen to unshareBoard event from any client, emit unsharingBoard event to all other clients/all clients
+
         
         // start up the server
         log.info("Whiteboard Server starting up");
